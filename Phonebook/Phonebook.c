da39560e6781c6f6b67440bf81324c98c08eb245
Mayurisangidwar <86296136+Mayurisangidwar@users.noreply.github.com>
2021-06-24 14:51:54 +0530

    Dummy Project Files Added.

---
+++ Phonebook/Phonebook.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void AddContact()
+{
+
+}
+void DisplayContact()
+{
+
+}
+void SearchContact()
+{
+
+}
+void DeleteContact()
+{
+
+}
+void EditContact()
+{
+
+}
+int main()
+{
+	printf("\nPhonebook");
+	return 0;
+}
